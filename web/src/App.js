abd3ccd110f45d800ddf150fc8a75b0471385c36
Yasiru Ediriweera <143874761+yashh123456789@users.noreply.github.com>
2024-06-30 02:27:58 +0530

    create navbar

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import  {NavBar} from "../src/components/navbar/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
